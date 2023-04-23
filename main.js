2ee94ee2b9d4562cc8c9f0631af09a2859639be3
Jiveshpp1 <89949407+Jiveshpp1@users.noreply.github.com>
2023-04-23 12:29:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqGRgSlLxmq9Pp9-O6yJgipnD0CuvfoMw",
+    authDomain: "kwitter-project-db8d3.firebaseapp.com",
+    projectId: "kwitter-project-db8d3",
+    storageBucket: "kwitter-project-db8d3.appspot.com",
+    messagingSenderId: "789666006688",
+    appId: "1:789666006688:web:4ddf980864794852876bc5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
